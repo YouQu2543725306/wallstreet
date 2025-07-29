d00465f043fd6434f4cd97651d9b8d415afad72d
boya wang <boya.wang@anu.edu.au>
2025-07-29 16:07:43 +0800

    try running cards version

--- backend/supabase/client.js
+++ backend/supabase/client.js
@@ -12,19 +12,13 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
 
-module.exports = supabase;
-
-//  test
-// async function fetchCharacters() {
-//     const { data, error } = await supabase
-//       .from('cards')
-//       .select();
-  
-//     if (error) {
-//       console.error('search error:', error);
-//     } else {
-//       console.log('search result:', data);
-//     }
-//   }
-  
-//   fetchCharacters();
+// 获取所有卡片数据的接口函数
+export async function getCards() {
+  const { data, error } = await supabase
+    .from('cards')
+    .select();
+  if (error) {
+    throw error;
+  }
+  return data;
+}
